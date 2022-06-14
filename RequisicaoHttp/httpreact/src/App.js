fa92803debdaf8e926aeb27bbe4e2e849064a803
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-06-14 01:29:53 -0300

    criando função fechtData async

--- RequisicaoHttp/httpreact/src/App.js
+++ RequisicaoHttp/httpreact/src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
 
+import { useState, useEffect } from "react";
+
+const url = "http://localhost/3000/products";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // resgatando dados
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fetchData();
+  }, [setProducts]);
+
+  console.log(products);
+
   return (
     <div className="App">
       <h1>Lista De Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - r$: {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
