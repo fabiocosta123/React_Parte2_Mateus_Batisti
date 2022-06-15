3a9943e89d47f9a8ddd5948d1769ee10041dd236
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-06-15 02:32:47 -0300

    criando função handleSubmit para add de produtos e metodo post

--- RequisicaoHttp/httpreact/src/App.js
+++ RequisicaoHttp/httpreact/src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 
-const url = "http://localhost/3000/products";
+const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   // resgatando dados
 
@@ -20,7 +23,24 @@ function App() {
     fetchData();
   }, [setProducts]);
 
-  console.log(products);
+  // add produtos
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const products = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(products),
+    });
+    console.log(products);
+  };
 
   return (
     <div className="App">
@@ -32,6 +52,29 @@ function App() {
           </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar Produto" />{" "}
+        </form>
+      </div>
     </div>
   );
 }
