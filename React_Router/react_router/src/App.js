7a9f5e54d05e2054f2b38455cf15e79e49824ebc
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-06-23 01:51:06 -0300

    alterando package.json, incluindo item no db

--- React_Router/react_router/src/App.js
+++ React_Router/react_router/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./Components/Navbar.css";
+
+// 1 - config react router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+
+//  Components
+import { Navbar } from "./Components/Navbar";
+
+//  Components
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
