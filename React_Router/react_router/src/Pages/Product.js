f88fa74406dbe1e936f6f21e166661c8ca127c00
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-06-25 23:25:08 -0300

    criando carregamento de dados dinamicos

--- React_Router/react_router/src/Pages/Product.js
+++ React_Router/react_router/src/Pages/Product.js
@@ -1,11 +1,28 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
   // rota dinamica
   const { id } = useParams();
+
+  // carregamento dado individual
+  const url = "http://localhost:3000/products/" + id;
+
+  const { data: product, loading, error } = useFetch(url);
+
+  console.log(product);
+
   return (
     <>
-      <h2>ID do produto: {id}</h2>;
+      <h2>ID do produto: {id}</h2>
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h1> {product.name}</h1>
+          <p>{product.price}</p>
+        </div>
+      )}
     </>
   );
 };
