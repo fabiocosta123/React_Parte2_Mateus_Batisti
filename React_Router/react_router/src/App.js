39b6d3cb1e52be4e44f348f968e6353e4d358ca7
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-06-26 01:04:40 -0300

    criando pagina de busca <useParams> e <useSearchParams>

--- React_Router/react_router/src/App.js
+++ React_Router/react_router/src/App.js
@@ -9,9 +9,12 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Product from "./Pages/Product";
 import Info from "./Pages/Info";
+import Search from "./Pages/Search";
+import NotFound from "./Pages/NotFound";
 
 //  Components
 import { Navbar } from "./Components/Navbar";
+import SearchForm from "./Components/SearchForm";
 
 //  Components
 
@@ -21,11 +24,14 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         <Navbar />
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products/:id" element={<Product />} />
           <Route path="/products/:id/Info" element={<Info />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
--- React_Router/react_router/src/Components/Navbar.js
+++ React_Router/react_router/src/Components/Navbar.js
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/About">About</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/About">About</Link>*/}
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/About">Sobre</NavLink>
     </nav>
   );
 };
